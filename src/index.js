20be0ba6d83902e275b645c2509360645699e591
Harshad Naik <naikharshad01@gmail.com>
2020-10-14 12:34:21 +0530

    asset management implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from "lodash";
+import "./styles.css";
+import Icon from "./dummy.png";
 
 const component = () => {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
   return element;
 };
 
